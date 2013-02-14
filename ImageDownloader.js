13ca6be14b75a8490fc74a3636ed6a2bf2803b39
Oscar Sobrevilla <oscar.sobrevilla@gmail.com>
2013-02-14 15:52:43 -0500

    add script beta

---
+++ ImageDownloader.js
@@ -0,0 +1,74 @@
+/* !JavaScript Image Downloader (Cancelable)
+ * Copyright 2013 Oscar Sobrevilla (oscar.sobrevilla@gmail.com)
+ * Released under the MIT and GPL licenses.
+ * version 0.1 beta
+ */
+
+"use strict";
+
+
+var ImageDownloader = (function (win) {
+
+  var _abort = function (iframe) {
+    var cw = iframe.contentWindow;
+    if (cw.stop) {
+      cw.stop();
+    } else {
+      cw = iframe.contentDocument;
+      cw.execCommand && cw.execCommand('Stop', false);
+    }
+    iframe && iframe.parentNode.removeChild(iframe);
+    delete iframe;
+  };
+  return {
+
+    /**
+     * Begin download image.
+     * @param src {String} 
+     * @param callback {Function}
+     * - return object 
+     *    - image:HTMLImageElement
+     *    - abort: function 
+     */
+
+    'load': function (src, callback) {
+      var iframe = document.createElement('iframe'),
+        img = new Image(),
+        doc;
+      iframe.style.display = 'none';
+      iframe.setAttribute('src', 'about:blank');
+      iframe.onload = function () {
+        doc = iframe.contentDocument || iframe.contentWindow.document;
+        img.onload = function () {
+          callback && callback.call(this, this);
+          _abort(iframe);
+        };
+        doc.body.appendChild(img);
+        img.src = src;
+      };
+      win.document.body.appendChild(iframe);
+      return {
+        'image': img,
+        'abort': function () {
+          _abort(iframe);
+          return img;
+        }
+      }
+    }
+  };
+}(window));
+
+
+
+/* ------ Use example ------ */
+
+var src = "http://www.hdwallpapersarena.com/wp-content/uploads/2012/07/Download-Hulk-HD-Wallpaper-For-Computer.jpg?x=" + Math.random() * 1000;
+// DownloadImage
+var obj = ImageDownloader.load(src, function () {
+  console.log(this, arguments);
+});
+// We waited 2 seconds to abort the download.
+setTimeout(function () {
+  var img = obj.abort();
+  console.log("abort src: ", img.src);
+}, 2000);
