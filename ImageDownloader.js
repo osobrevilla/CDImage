bb115cab841973369c6e03ad87dfbae244b8a862
Oscar Sobrevilla <oscar.sobrevilla@gmail.com>
2013-04-15 13:55:34 -0500

    fix bugs, reference objects in set null

--- ImageDownloader.js
+++ ImageDownloader.js
@@ -1,40 +1,44 @@
 /* !JavaScript Image Downloader (Cancelable)
-* Copyright 2013 Oscar Sobrevilla (oscar.sobrevilla@gmail.com)
-* Released under the MIT and GPL licenses.
-* version 0.1.8 beta
-*/
-var ImageDownloader = (function (win) {
-    
+ * Copyright 2013 Oscar Sobrevilla (oscar.sobrevilla@gmail.com)
+ * Released under the MIT and GPL licenses.
+ * version 0.1.8 beta
+ */
+var ImageDownloader = (function (win, doc, undefined) {
+  var _stopAndRemove = function (iframe) {
+    if (!iframe) return null;
+    try {
+      var cw = iframe.contentWindow;
+      if (cw.stop) {
+        cw.stop();
+      } else {
+        cw = iframe.contentDocument;
+        cw.execCommand && cw.execCommand('Stop', false);
+      }
+      iframe.parentNode.removeChild(iframe);
+      return true;
+    } catch (e) {
+      throw e.toString();
+      return false;
+    }
+  };
   return {
     /**
      * Begin download image.
      * @param src {String} 
      * @param callback {Function}
      * - return object 
-     *    - image:HTMLImageElement
-     *    - abort: function 
+     *    - abort: {function}
+     *      - return {String} src
      */
     'load': function (src, callback) {
-
       var iframe = win.document.createElement('iframe'),
-        _abort = function(){
-          if ( !iframe )
-            return;
-          var cw = iframe.contentWindow;
-          if(cw.stop) {
-            cw.stop();
-          } else {
-            cw = iframe.contentDocument;
-            cw.execCommand && cw.execCommand('Stop', false);
-          }
-          iframe.parentNode.removeChild(iframe);
-          iframe = null;
-          return src;
+        _abort = function () {
+          var res = _stopAndRemove(iframe);
+          if (res) iframe = null;
+          return res === true ? src : res;
         };
-
       iframe.style.display = 'none';
       iframe.setAttribute('src', 'about:blank');
-
       iframe.onload = function () {
         this.onload = null;
         var img = new win.Image();
@@ -46,12 +50,10 @@ var ImageDownloader = (function (win) {
         (iframe.contentDocument || iframe.contentWindow.document).body.appendChild(img);
         img.src = src;
       };
-      
-      win.document.body.appendChild(iframe);
-      
+      doc.body.appendChild(iframe);
       return {
         'abort': _abort
       };
     }
   };
-}(window));
+}(window, window.document));
