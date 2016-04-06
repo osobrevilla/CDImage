8a95ca1f9b98f06470b9f0a8b61227863f300c46
Oscar Sobrevilla <oscar.sobrevilla@gmail.com>
2016-04-06 12:48:30 -0500

    Update CDImage.js

--- CDImage.js
+++ CDImage.js
@@ -1,77 +1,77 @@
-    (function(doc) {
+(function(doc) {
 
-        'use strict';
+    'use strict';
 
-        function _stopAndRemove(iframe) {
-            if (iframe) {
-                try {
-                    var c = iframe.contentWindow;
-                    c && c.stop ? c.stop() :
-                        c = iframe.contentDocument,
-                    c && c.execCommand && c.execCommand('Stop', false);
-                    iframe.parentNode.removeChild(iframe);
-                    return true;
-                } catch (e) {
-                    throw e.toString();
-                }
+    function _stopAndRemove(iframe) {
+        if (iframe) {
+            try {
+                var c = iframe.contentWindow;
+                c && c.stop ? c.stop() :
+                    c = iframe.contentDocument,
+                c && c.execCommand && c.execCommand('Stop', false);
+                iframe.parentNode.removeChild(iframe);
+                return true;
+            } catch (e) {
+                throw e.toString();
             }
         }
+    }
 
-        this.CDImage = {
-            /**
-             * Begin download image.
-             * @param src {String}
-             * @param callback {Function}
-             * @expose
-             * @return {Object}
-             */
-            load: function(src, callback, crossOrigin) {
-              // crossOrigin values how "anonymous" or "use-credentials"
-                var idoc,
-                    iframe = doc.createElement('iframe');
-                iframe.style.display = 'none';
-                doc.body.appendChild(iframe);
-                idoc = iframe.contentDocument || iframe.contentWindow.document;
-                idoc.domain = location.host;
-                idoc.open();
-                function _abort() {
-                    if (iframe){
-                        iframe.removeEventListener ?
-                        iframe.removeEventListener('load', loaded, false) : iframe.detachEvent('onload', loaded);
-                    }
-                    var res = _stopAndRemove(iframe);
-                    if (res) iframe = null;
-                    idoc = null;
-                    return res === true ? src : res;
+    this.CDImage = {
+        /**
+         * Begin download image.
+         * @param src {String}
+         * @param callback {Function}
+         * @expose
+         * @return {Object}
+         */
+        load: function(src, callback, crossOrigin) {
+          // crossOrigin values how "anonymous" or "use-credentials"
+            var idoc,
+                iframe = doc.createElement('iframe');
+            iframe.style.display = 'none';
+            doc.body.appendChild(iframe);
+            idoc = iframe.contentDocument || iframe.contentWindow.document;
+            idoc.domain = location.host;
+            idoc.open();
+            function _abort() {
+                if (iframe){
+                    iframe.removeEventListener ?
+                    iframe.removeEventListener('load', loaded, false) : iframe.detachEvent('onload', loaded);
                 }
-
-                function loaded() {
-                    this.removeEventListener ?
-                        this.removeEventListener('load', loaded, false) : this.detachEvent('onload', loaded);
-                    var img = new Image();
-                        if (crossOrigin)
-                            img.crossOrigin = crossOrigin;
-                    img.onload = function() {
-                        _abort();
-                        this.parentNode.removeChild(img);
-                        callback && callback(src, {
-                            width: img.width,
-                            height: img.height
-                        }, img);
-                        this.onload = null;
-                        loaded = null;
-                    };
-                    idoc.body.appendChild(img);
-                    img.src = src;
-                }
-                iframe.addEventListener ?
-                    iframe.addEventListener('load', loaded, false) : iframe.attachEvent('onload', loaded);
-                idoc.close();
-                return {
-                    /** @expose **/
-                    abort: _abort
-                };
+                var res = _stopAndRemove(iframe);
+                if (res) iframe = null;
+                idoc = null;
+                return res === true ? src : res;
             }
-        };
 
-    }.call(window, window.document));
+            function loaded() {
+                this.removeEventListener ?
+                    this.removeEventListener('load', loaded, false) : this.detachEvent('onload', loaded);
+                var img = new Image();
+                    if (crossOrigin)
+                        img.crossOrigin = crossOrigin;
+                img.onload = function() {
+                    _abort();
+                    this.parentNode.removeChild(img);
+                    callback && callback(src, {
+                        width: img.width,
+                        height: img.height
+                    }, img);
+                    this.onload = null;
+                    loaded = null;
+                };
+                idoc.body.appendChild(img);
+                img.src = src;
+            }
+            iframe.addEventListener ?
+                iframe.addEventListener('load', loaded, false) : iframe.attachEvent('onload', loaded);
+            idoc.close();
+            return {
+                /** @expose **/
+                abort: _abort
+            };
+        }
+    };
+
+}.call(window, window.document));
